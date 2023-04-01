52fd35b75e55da5234ab3b6768999db48e317583
padamchopra <jb.padamchopra@gmail.com>
2023-04-01 11:25:04 -0400

    add meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,14 @@ export default function Document() {
       <title>
         Padam Chopra
       </title>
+      <meta name="description" content="Hi, I'm Padam 👋🏼. Building digital experiences, specializing in Android, Flutter, and React." />
+      <link rel="icon" href="/favicon.png" />
+      {/* Insert all twitter meta tags for url preview of type summary */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@PadamChopra_" />
+      <meta name="twitter:creator" content="@PadamChopra_" />
+      <meta name="twitter:title" content="Padam Chopra" />
+      <meta name="twitter:description" content="Hi, I'm Padam 👋🏼. Building digital experiences, specializing in Android, Flutter, and React." />
       <Head />
       <body>
       {/* dark:bg-black dark:text-white */}
